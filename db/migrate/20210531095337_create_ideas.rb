cdf0ce1aab2268b80744865e7bed9441f1bada68
yamaryo0250 <ryo.yamagiwa0250@gmail.com>
2021-05-31 18:58:08 +0900

    categoryテーブル、ideaテーブル作成

---
+++ db/migrate/20210531095337_create_ideas.rb
@@ -0,0 +1,9 @@
+class CreateIdeas < ActiveRecord::Migration[6.0]
+  def change
+    create_table :ideas do |t|
+      t.references :category, null: false, foreign_key: true
+      t.text :body, null: false
+      t.timestamps
+    end
+  end
+end
