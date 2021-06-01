08f001a84b1449113de664212b4e198198a28bda
yamaryo0250 <ryo.yamagiwa0250@gmail.com>
2021-06-01 16:57:47 +0900

    バリデーション追加

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -6,5 +6,20 @@ class IdeasController < ApplicationController
   end
 
   def create
+    @category = Category.create(category_params)
+    @idea = Idea.create(idea_params)
+    redirect_to root_path
   end
+
+
+  private
+  
+  def category_params
+    params.permit(:name)
+  end
+
+  def idea_params
+    params.permit(:body).merge(category_id: @category.id)
+  end
+
 end
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,3 +1,5 @@
 class Idea < ApplicationRecord
   belongs_to :category
+
+  validates :body, presence: true
 end
